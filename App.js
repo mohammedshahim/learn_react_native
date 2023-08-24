d471d09c48186a920fa0d38dcf05a304884bb3e8
Shahim <muhdshahim786@gmail.com>
2023-08-24 17:15:06 +0400

    working with flex

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text>Hello world!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.textInput} />
+        <Button title="Add Goal" />
       </View>
-      <Text>Hello world!</Text>
-      <Button title="Click me" onPress={() => alert("Button clicked")} />
+      <View>
+        <Text>Goal List...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
