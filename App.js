299549ff485a5479c76f8e386506bc82b2124f86
Shahim <muhdshahim786@gmail.com>
2023-08-25 10:43:43 +0400

    using useState to manange a list

--- App.js
+++ App.js
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoalText(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoalText]);
+  };
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.textInput} />
-        <Button title="Add Goal" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <View style={styles.button}>
+          <Button title="Add Goal" color={"white"} onPress={addGoalHandler} />
+        </View>
       </View>
-      <View>
-        <Text>Goal List...</Text>
+      <View style={styles.goalsContainer}>
+        <Text>
+          {courseGoals.map((goal, idx) => (
+            <Text key={idx}>
+              {goal}
+              {"\n"}
+            </Text>
+          ))}
+        </Text>
       </View>
     </View>
   );
@@ -16,17 +40,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 4,
+  },
+  button: {
+    backgroundColor: "blue",
+    color: "white",
+    borderRadius: 4,
   },
 });
